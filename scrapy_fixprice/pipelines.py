000049f57cce82d5db9080e4c8df9d928166b728
valed-dm <dmvaled@gmail.com>
2024-07-24 09:29:55 +0300

    pipelines.py now process items for sorted output

--- scrapy_fixprice/pipelines.py
+++ scrapy_fixprice/pipelines.py
@@ -2,30 +2,68 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-import json
+import logging
 import os
+import json
 
-# useful for handling different item types with a single interface
+import scrapy
+
+logging.basicConfig(
+    filename='scrapy_spider.log',
+    filemode='w',
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.DEBUG
+)
+
+
+class ItemFilterPipeline:
+    def __init__(self, start_urls):
+        self.categories = [url.split('/')[-2] for url in start_urls]
+        logging.info(f"Categories to filter: {self.categories}")
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        start_urls = crawler.spider.start_urls
+        return cls(start_urls)
+
+    def process_item(self, item, spider):
+        item_url = item['url']
+        if any(category in item_url for category in self.categories):
+            logging.info(f"Item accepted: {item_url}")
+            return item
+        else:
+            logging.warning(f"Item dropped: {item_url}")
+            raise scrapy.exceptions.DropItem(f"Item does not belong to any category: {item['url']}")
 
 
 class MultiFilePipeline:
-    def __init__(self):
+    def __init__(self, start_urls):
+        self.feed_uri = None
         self.files = {}
+        self.categories = [url.split('/')[-2] for url in start_urls]
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        start_urls = crawler.spider.start_urls
+        return cls(start_urls)
 
     def open_spider(self, spider):
-        pass
+        self.feed_uri = './output/'  # Set your output directory
+        if not os.path.exists(self.feed_uri):
+            os.makedirs(self.feed_uri)
+        for category in self.categories:
+            file_path = os.path.join(self.feed_uri, f"{category}.jsonl")
+            self.files[category] = open(file_path, 'w', encoding='utf8')
 
     def close_spider(self, spider):
         for file in self.files.values():
             file.close()
 
     def process_item(self, item, spider):
-        category_url = item['url']
-        file_name = category_url.split('/')[-2] + ".json"
-        if file_name not in self.files:
-            file_path = os.path.join(spider.settings.get('FEED_URI', '.'), file_name)
-            self.files[file_name] = open(file_path, 'w')
-
-        line = json.dumps(dict(item)) + "\n"
-        self.files[file_name].write(line)
-        return item
+        item_url = item['url']
+        for category in self.categories:
+            if category in item_url:
+                line = json.dumps(dict(item), ensure_ascii=False) + "\n"
+                self.files[category].write(line)
+                return item
+        return item  # sorting done in ItemFilterPipeline, to avoid NoneType errors;
